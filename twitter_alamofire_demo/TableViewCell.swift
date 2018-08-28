6500a8a5009241063b91aefb1c654f369c90ba02
Mayank Gandhi <mayank.gandhi@learner.manipal.edu>
2018-08-27 21:18:25 -0700

    inital commit - login working

---
+++ twitter_alamofire_demo/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  twitter_alamofire_demo
+//
+//  Created by Mayank Gandhi on 8/27/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
